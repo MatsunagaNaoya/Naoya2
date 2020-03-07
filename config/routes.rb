75b2873d894fd4344c2666a718ad3b13dab57b89
EC2 Default User <ec2-user@ip-172-31-47-102.ap-northeast-1.compute.internal>
2020-03-07 11:40:49 +0000

    Finish add-user-attendances

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   resources :users do
     member do
       get 'edit_basic_info'
       patch 'update_basic_info'
     end
+    resources :attendances, only: :update
   end
 end
 
